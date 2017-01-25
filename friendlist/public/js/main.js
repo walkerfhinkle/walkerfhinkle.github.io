f26e1bb31519bea98c95707b3927c8721aa3a096
King James <King James>
2017-01-24 20:38:36 -0500

    added images and created new entries to project section

--- friendlist/public/js/main.js
+++ friendlist/public/js/main.js
@@ -10,7 +10,7 @@ var heroTemplate = "" +
 	"<p><strong>Name:</strong> {{name}}</p>" +
 	"<p><strong>Ability:</strong> {{ability}}</p>" +
 	"<p><strong>City:</strong> {{city}}</p>" +
-	"<button id='{{id}}' class='remove'>X</button>" +
+	"<button id='{{id}}' class='remove' style='color: white'>X</button>" +
 	"</li>";
 	//DRY - Don't Repeat Yourself
 
@@ -67,18 +67,32 @@ $(document).ready(function() {
 					alert('Error adding new hero');
 				}
 
-			})
+			});
 
-		})
+		});
 
-	//DELETE to dump 'em
-	$.ajax({
-  		type: 'DELETE',
-  		url: 'http://rest.learncode.academy/api/whinkle/heroes',
-  			success: function() {
-    			//no data
-    			console.log('Hero deleted successfully!');
-  			}
+	//.delegate allows you to remove items that were loaded by other students
+	//look into the friendTemplate from above the button has the class="remove"
+	//in it.
+	$heroes.delegate('.remove', 'click', function(){
+
+		//this should look a little familiar.  It is saying whatever was clicked with the
+		//class of .remove then $li is the closest li to that particular button.
+		var $li = $(this).closest('li');
+		//AJAX DELETE Function - click the .remove class button and the id identifies what to delete
+		$.ajax({
+			type: 'DELETE',
+			//$(this).attr('id') is that button's id we set up the template.
+			//it is the same thing as copying an id like we did in postman
+			//and running the delete request at the API.
+			url: 'http://rest.learncode.academy/api/whinkle/heroes/' + $(this).attr('id'),
+			success: function(){
+				//once that happens we fade out the li and then we remove
+				//it from the DOM.
+				$li.fadeOut(300, function(){
+					$(this).remove();
+				});
+			}
+		});
 	});
-
 });
