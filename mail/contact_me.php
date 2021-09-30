874facde2baeb3fbcec9007feb3f33b8f421b885
Walker <whinkle653@gmail.com>
2021-09-30 16:44:43 -0400

    Update contact_me.php
    Updated PHP

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -11,16 +11,16 @@ if(empty($_POST['name'])      ||
    }
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
-$email = strip_tags(htmlspecialchars($_POST['email']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
 $to = 'https://formspree.io/whinkle653@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your portfolio page.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
+$email_body = "You have received a new message from your portfolio page.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@walkerfhinkle.github.io\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
-return true;
+return true;         
 ?>
